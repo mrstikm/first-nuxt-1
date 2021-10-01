c42ad69c215208a286aae0d4a15be8acd0b63996
mrstikm <mrstik.martin@gmail.com>
2021-10-01 11:30:22 +0200

    first working

--- components/HeroSlider.vue
+++ components/HeroSlider.vue
@@ -1,8 +1,12 @@
 <template>
     <section class="relative overflow-hidden">
-        <img class="hero" src="https://via.placeholder.com/1920x1080" alt="">
-        <div class="slides absolute top-1/2 transform -translate-y-1/2 left-1/2 flex overflow-x-hidden">
-            <HeroSingleSlide v-for="img, index in imgSrcs" :key="index" :src="img" />
+        <img class="hero" :src="activeItem.img" alt="">
+        <div class="absolute top-1/2 transform -translate-y-1/2 left-1/2 flex overflow-x-hidden">
+            <button class="self-end border border-black mr-1" @click="prevSlide"><img class="max-w-min" src="~/static/images/left.svg" alt="Left"></button>
+            <button class="self-end border border-black" @click="nextSlide"><img class="max-w-min" src="~/static/images/right.svg" alt="Right"></button>
+            <div class="slides flex">
+                <HeroSingleSlide v-for="ref,index in references" :key="ref.key" :src="ref.tmb" :class="{ 'active': index === 0 }" />
+            </div>
         </div>
     </section>
 </template>
@@ -15,15 +19,63 @@ export default {
     },
     data() {
         return {
-            imgSrcs: [
-                'https://via.placeholder.com/200x300',
-                'https://via.placeholder.com/200x300',
-                'https://via.placeholder.com/200x300',
-                'https://via.placeholder.com/200x300',
-                'https://via.placeholder.com/200x300',
-                'https://via.placeholder.com/200x300',
-            ]
+            references: [
+                {
+                    'key': 1,
+                    'img': 'https://via.placeholder.com/1920x1080/255',
+                    'tmb': 'https://via.placeholder.com/200x300/255',
+                },
+                {
+                    'key': 2,
+                    'img': 'https://via.placeholder.com/1920x1080/600',
+                    'tmb': 'https://via.placeholder.com/200x300/600',
+                },
+                {
+                    'key': 3,
+                    'img': 'https://via.placeholder.com/1920x1080/002012',
+                    'tmb': 'https://via.placeholder.com/200x300/002012',
+                },
+                {
+                    'key': 4,
+                    'img': 'https://via.placeholder.com/1920x1080/012304',
+                    'tmb': 'https://via.placeholder.com/200x300/012304',
+                },
+                {
+                    'key': 5,
+                    'img': 'https://via.placeholder.com/1920x1080/012304',
+                    'tmb': 'https://via.placeholder.com/200x300/012304',
+                },
+                {
+                    'key': 6,
+                    'img': 'https://via.placeholder.com/1920x1080/1024',
+                    'tmb': 'https://via.placeholder.com/200x300/1024',
+                }
+            ],
+            keyActive: 1
         }
-    }
+    },
+    computed: {
+        lastItem() {
+            return this.references.slice(-1)[0]
+        },
+        activeItem() {
+            return this.references.find( ref => {
+                return ref.key === this.keyActive
+            })
+        }
+    },
+    methods: {
+        prevSlide() {
+            const [first, ...rest] = this.references
+            this.keyActive = first.key
+            this.references = [...rest, first]
+        },
+        nextSlide() {
+            const lastItem = this.lastItem
+            this.keyActive = lastItem.key
+            this.references.pop()
+            this.references = [lastItem, ...this.references]
+        }
+    },
 }
 </script>
